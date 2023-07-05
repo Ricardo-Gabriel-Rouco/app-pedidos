1c67310515ada3dc6cc18d5143fc4b19620a5c00
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-07-05 16:47:43 -0300

    correciones deploy

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const server = require('./src/app')
 const { conn } = require('./src/db')
+const {PORT} = process.env
 
 conn.sync({ force: false, alter: true }).then(() => {
-  server.listen(5000, () => {
-    console.log('%s listening at 5000'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
