2f21016d41695ff76b9799acc4b02460230d059a
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-07-05 00:48:05 -0300

    modal con edicion de cantidad

--- client/appvero/src/components/ModalProducts/ModalProducts.jsx
+++ client/appvero/src/components/ModalProducts/ModalProducts.jsx
@@ -1,31 +1,96 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import PropTypes from "prop-types";
+import { Formik, Form as FormikForm, Field, ErrorMessage } from "formik";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import axios from "axios";
 
-// show puede ser solo un booleano y siempre es true cuando viene por prop
 function ModalProducts(props) {
   const { productId, name, quantity, show, onClose } = props;
+  const [initialQuantity, setInitialQuantity] = useState(quantity);
 
-  // useEffect(() => {
-  //   setShowModal(show)
-  // }, [show])
-  
+  const handleIncrement = () => {
+    setInitialQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (initialQuantity > 0) {
+      setInitialQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    if (!isNaN(newValue)) {
+      setInitialQuantity(newValue);
+    }
+  };
+
+  const saveChanges = async (productId, initialQuantity) => {
+    try {
+      await axios({
+        method: "put",
+        url: `/products/${productId}`,
+        data: {
+          quantity: initialQuantity,
+        },
+      });
+      onClose()
+      window.location.reload()
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-      <Modal show={show} onHide={onClose} key={productId}>
-        <Modal.Header closeButton>
-          <Modal.Title>{name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{quantity}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={onClose}>
-            Close
-          </Button>
-          <Button variant="info">
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    <Modal show={show} onHide={onClose} key={productId} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>{name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Formik initialValues={{ initialQuantity }}>
+          <FormikForm>
+            <Form.Group controlId="initialQuantity">
+              <Form.Label>Stock:</Form.Label>
+              <div className="d-flex align-items-center">
+                <Button variant="secondary" onClick={handleDecrement}>
+                  -
+                </Button>
+                <Field
+                  type="number"
+                  name="initialQuantity"
+                  className="form-control mx-2"
+                  value={initialQuantity}
+                  onChange={handleChange}
+                  onBlur={handleChange}
+                />
+                <Button variant="secondary" onClick={handleIncrement}>
+                  +
+                </Button>
+              </div>
+              <ErrorMessage
+                name="initialQuantity"
+                component="div"
+                className="text-danger"
+              />
+            </Form.Group>
+          </FormikForm>
+        </Formik>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={onClose}>
+          Close
+        </Button>
+        <Button
+          variant="info"
+          onClick={() => saveChanges(productId, initialQuantity)}
+          type="submit"
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
